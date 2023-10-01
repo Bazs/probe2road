ddc0f741ea6018cc0446f4d3793ecec510a1b557
Balazs Opra <balazs.opra@woven-planet.global>
2023-10-01 18:50:21 +0900

    Add comparison images with zoom, changelog

--- static/js/index.js
+++ static/js/index.js
@@ -19,3 +19,32 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.zoomable-image');
+  
+    function handleMouseMove(e) {
+      const rect = this.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top,
+            xPercent = Math.round((x / this.width) * 100),
+            yPercent = Math.round((y / this.height) * 100);
+  
+      this.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+    }
+  
+    images.forEach(img => {
+      img.addEventListener('click', function() {
+        if (this.style.transform === 'scale(1.5)') {
+          this.style.transform = 'scale(1)';
+          this.style.cursor = 'zoom-in';
+          this.removeEventListener('mousemove', handleMouseMove);
+        } else {
+          this.style.transform = 'scale(1.5)';
+          this.style.cursor = 'zoom-out';
+          this.addEventListener('mousemove', handleMouseMove);
+        }
+      });
+    });
+  });
+  
